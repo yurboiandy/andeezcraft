7b5554b6eab27513d2edb0ef94cf0aa2b8d8e564
PrestonT500 <preston.truong1@outlook.com>
2024-09-18 19:12:59 -0700

    Show mobile clients checkbox

---
+++ js/mobilecheck.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    function updateVisibility() {
+        var isChecked = $("#mobileCheckbox").is(":checked");
+        if (isChecked) {
+            $("#desktopVersion18").hide();
+            $("#mobileVersion18").show();
+            $("#desktopVersion152").hide();
+            $("#mobileVersion152").show();
+        } else {
+            $("#desktopVersion18").show();
+            $("#mobileVersion18").hide();
+            $("#desktopVersion152").show();
+            $("#mobileVersion152").hide();
+        }
+    }
+
+    function updateLinks() {
+        $(".version-link").each(function() {
+            var url = $(this).data("url");
+            $(this).attr("href", url);
+        });
+    }
+
+    $("#mobileCheckbox").on("change", function() {
+        updateVisibility();
+        updateLinks();
+    });
+
+    // Initialize visibility and links based on the checkbox state
+    updateVisibility();
+});
