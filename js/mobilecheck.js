69678c3c98c109cdbc8e338cb393858c456e632c
PrestonT500 <preston.truong1@outlook.com>
2024-09-22 19:25:11 -0700

    Fixed Mobile CheckBox

--- js/mobilecheck.js
+++ js/mobilecheck.js
@@ -6,11 +6,15 @@ $(document).ready(function() {
             $("#mobileVersion18").show();
             $("#desktopVersion152").hide();
             $("#mobileVersion152").show();
+            $("#desktopVersion19").hide();
+            $("#mobileVersion19").show();
         } else {
             $("#desktopVersion18").show();
             $("#mobileVersion18").hide();
             $("#desktopVersion152").show();
             $("#mobileVersion152").hide();
+            $("#desktopVersion19").show();
+            $("#mobileVersion19").hide();
         }
     }
 
