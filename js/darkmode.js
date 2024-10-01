2ec3f49cd3690060367258627ce17f838ce2ae34
css pro <qwertyum4675@proton.me>
2024-10-01 11:11:36 -0700

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,7 +1,7 @@
 function dMode(){
   var buttons = document.getElementsByTagName('button');
   var body = document.body;
-  buttons.style.background-color("#13f4f3");
-  body.style.color("f1f1f1");
-  body.style.background-color("3c3c3c");
+  buttons.style.backgroundcolor="#13f4f3";
+  body.style.color="f1f1f1";
+  body.style.backgroundcolor="3c3c3c";
 }
