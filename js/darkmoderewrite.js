7fe86c2681d642c8ff2a8c3765378779e1752703
css pro <qwertyum4675@proton.me>
2024-10-02 14:28:44 -0700

    I hope it works🙏

--- js/darkmoderewrite.js
+++ js/darkmoderewrite.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
         if (isChecked) {
             const body = document.querySelector('body');
             body.style.backgroundColor = '#404040';
-
+            body.style.color = '#f1f1f1';
+            
             const text = document.querySelector('.footer');
             text.style.backgroundColor = 'grey';
-            
-            const body = document.querySelector('text');
-            body.style.color = '#f1f1f1';
 
             const body = document.querySelector('button');
             body.style.backgroundColor = '#13f4f3';
@@ -17,12 +15,10 @@ $(document).ready(function() {
         } else {
             const body = document.querySelector('body');
             body.style.backgroundColor = '#f1f1f1';
-
+            body.style.color = 'black';
+            
             const text = document.querySelector('.footer');
             text.style.backgroundColor = '#ddd';
-
-            const body = document.querySelector('text');
-            body.style.color = 'black';
 
             const body = document.querySelector('button');
             body.style.backgroundColor = '#4caf50';
