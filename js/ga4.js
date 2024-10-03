a8205f5a68cdfc00d7a8cf062ae2100df1e13ff9
Preston <PrestonT500WasTaken@proton.me>
2024-10-02 17:05:22 -0700

    Added detect url so the ga4 doesn't triple count and stuff

--- js/ga4.js
+++ js/ga4.js
@@ -1,49 +1,69 @@
 function addGoogleAnalyticsTags() {
-    // GitHub Pages
-    var githubAnalyticsScript = document.createElement('script');
-    githubAnalyticsScript.async = true;
-    githubAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-C44R8LD9MC';
-    document.head.appendChild(githubAnalyticsScript);
+    var currentDomain = window.location.hostname;
 
-    var githubScriptContent = document.createElement('script');
-    githubScriptContent.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-C44R8LD9MC');
-    `;
-    document.head.appendChild(githubScriptContent);
+    if (currentDomain === "eaglercraftx1-8.github.io") {
+        // GitHub Pages Analytics
+        var githubAnalyticsScript = document.createElement('script');
+        githubAnalyticsScript.async = true;
+        githubAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-C44R8LD9MC';
+        document.head.appendChild(githubAnalyticsScript);
 
-    // Netlify
-    var netlifyAnalyticsScript = document.createElement('script');
-    netlifyAnalyticsScript.async = true;
-    netlifyAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-0R2ZXFSYVT';
-    document.head.appendChild(netlifyAnalyticsScript);
+        var githubScriptContent = document.createElement('script');
+        githubScriptContent.innerHTML = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C44R8LD9MC');
+        `;
+        document.head.appendChild(githubScriptContent);
 
-    var netlifyScriptContent = document.createElement('script');
-    netlifyScriptContent.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-0R2ZXFSYVT');
-    `;
-    document.head.appendChild(netlifyScriptContent);
+    } else if (currentDomain === "eaglercraftx1-8.netlify.app") {
+        // Netlify Analytics
+        var netlifyAnalyticsScript = document.createElement('script');
+        netlifyAnalyticsScript.async = true;
+        netlifyAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-0R2ZXFSYVT';
+        document.head.appendChild(netlifyAnalyticsScript);
 
-    // Render
-    var renderAnalyticsScript = document.createElement('script');
-    renderAnalyticsScript.async = true;
-    renderAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-MND1TVBSXV';
-    document.head.appendChild(renderAnalyticsScript);
+        var netlifyScriptContent = document.createElement('script');
+        netlifyScriptContent.innerHTML = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0R2ZXFSYVT');
+        `;
+        document.head.appendChild(netlifyScriptContent);
 
-    var renderScriptContent = document.createElement('script');
-    renderScriptContent.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-MND1TVBSXV');
-    `;
-    document.head.appendChild(renderScriptContent);
+    } else if (currentDomain === "eaglercraftx1-8.onrender.com") {
+        // Render Analytics
+        var renderAnalyticsScript = document.createElement('script');
+        renderAnalyticsScript.async = true;
+        renderAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-MND1TVBSXV';
+        document.head.appendChild(renderAnalyticsScript);
+
+        var renderScriptContent = document.createElement('script');
+        renderScriptContent.innerHTML = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MND1TVBSXV');
+        `;
+        document.head.appendChild(renderScriptContent);
+
+    } else {
+        // Else (Fallback) Analytics
+        var fallbackAnalyticsScript = document.createElement('script');
+        fallbackAnalyticsScript.async = true;
+        fallbackAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-81F615LDEZ';
+        document.head.appendChild(fallbackAnalyticsScript);
+
+        var fallbackScriptContent = document.createElement('script');
+        fallbackScriptContent.innerHTML = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-81F615LDEZ');
+        `;
+        document.head.appendChild(fallbackScriptContent);
+    }
 }
-
-// Call the function to add Google Analytics tags
 addGoogleAnalyticsTags();
